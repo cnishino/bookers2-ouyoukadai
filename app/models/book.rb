5703a0f5658d2f6d8176746d2ab937a41c9b87ea
cnishino <cnishino19820915@icloud.com>
2022-05-19 08:37:30 +0000

    [fix]7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -20,8 +20,8 @@ class Book < ApplicationRecord
 
   scope :created_today, -> { where(created_at: Time.zone.now.all_day)} #今日の投稿数　scope :スコープの名前, -> { 条件式 }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day)} #昨日の投稿数
-  scope :created_this_week, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) } #今週の投稿数
-  scope :created_last_week, -> { where(created_at: 1.week.ago.all_day)} #先週の投稿数
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週の投稿数
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.beginning_of_day)} #先週の投稿数
 
 
 
