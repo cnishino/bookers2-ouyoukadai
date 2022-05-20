99bea3866b665f3ba9c2f1b64d281ff3b295b91c
cnishino <cnishino19820915@icloud.com>
2022-05-20 10:51:14 +0000

    [finish]8b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,13 @@ class UsersController < ApplicationController
     @book = Book.new
     @today_book = @books.created_today #今日の投稿数
     @yesterday_book = @books.created_yesterday #昨日の投稿数
+    @day2_ago_boo = @books.created_2day_ago #2日前の投稿数
+    @day3_ago_book = @books.created_3day_ago #3日前の投稿数
+    @day4_ago_book = @books.created_4day_ago #4日前の投稿数
+    @day5_ago_book = @books.created_5day_ago #5日前の投稿数
+    @day6_ago_book = @books.created_6day_ago #6日前の投稿数
     @this_week_book = @books.created_this_week #今週の投稿数
     @last_week_book = @books.created_last_week #先週の投稿数
-
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -20,6 +20,11 @@ class Book < ApplicationRecord
 
   scope :created_today, -> { where(created_at: Time.zone.now.all_day)} #今日の投稿数　scope :スコープの名前, -> { 条件式 }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day)} #昨日の投稿数
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day)} #2日前の投稿数
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day)} #3日前の投稿数
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day)} #4日前の投稿数
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day)} #5日前の投稿数
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day)} #6日前の投稿数
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週の投稿数
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.beginning_of_day)} #先週の投稿数
 
